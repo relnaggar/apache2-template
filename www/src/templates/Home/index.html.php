53a6f0fae9fc17ed0f84b7563b699b4467b7cf67
relnaggar <ramsey.el-naggar@outlook.com>
2024-11-16 14:18:59 +0000

    add sqlite db

--- www/src/templates/Home/index.html.php
+++ www/src/templates/Home/index.html.php
@@ -1,4 +1,20 @@
 <div class="container">
   <?= 'Hello World!' ?>
   <i class="bi bi-bootstrap"></i>
+  <?php
+    $databaseWorking = false;
+    try {
+      $pdo = new PDO('sqlite:/var/db/database.sqlite');
+      $databaseWorking = true;
+    } catch (PDOException $e) {
+      $error = $e->getMessage();
+    }
+  ?>
+  <p>
+    <?php if ($databaseWorking): ?>
+      Database is working!
+    <?php else: ?>
+      Database isn't working! Error: <?= $error ?>
+    <?php endif; ?>
+  </p>
 </div>
